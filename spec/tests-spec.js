abb61d71947fc2d657a64b31369da2f60b25b041
Brendan Shea <padma@email.com>
2018-10-05 13:59:16 -0700

    Add global variables to make code DRY.

--- spec/tests-spec.js
+++ spec/tests-spec.js
@@ -2,9 +2,16 @@ import { UserAge } from './../src/backend.js';
 
 describe('User Age', function() {
 
+  beforeEach(function() {
+    userAge = new UserAge(28);
+    userAgeInDays = userAge.age * 365;
+  });
+
   it('should calculate user age', function() {
-    let userAge = new UserAge(28);
     expect(userAge.age).toEqual(28);
   });
 
+  it('should multiply user age by 365', function () {
+    expect(userAgeInDays).toEqual(10220);
+  });
 });
