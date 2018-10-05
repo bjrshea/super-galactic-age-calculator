1a34350700ab155b2bc65f7337cfe6d8acaa2335
Brendan Shea <padma@email.com>
2018-10-05 13:02:57 -0700

    Add ability to submit age and calculate it.

--- src/main.js
+++ src/main.js
@@ -4,4 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function() {
+  $("#form").submit(function(event) {
+    event.preventDefault();
+    let userInput = parseInt($("#user-input").val());
+    let userAge = userInput * 365;
+  });
 });
