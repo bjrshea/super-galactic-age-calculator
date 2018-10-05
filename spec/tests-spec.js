693e65d0d1b4ee12298b63b39318ce64fed29a14
Brendan Shea <padma@email.com>
2018-10-05 14:18:56 -0700

    Create mercury calculator test that fails.

--- spec/tests-spec.js
+++ spec/tests-spec.js
@@ -2,16 +2,22 @@ import { UserAge } from './../src/backend.js';
 
 describe('User Age', function() {
 
-  beforeEach(function() {
-    userAge = new UserAge(28);
-    userAgeInDays = userAge.age * 365;
-  });
-
   it('should calculate user age', function() {
+    let userAge = new UserAge(28);
     expect(userAge.age).toEqual(28);
   });
 
   it('should multiply user age by 365', function () {
+    let userAge = new UserAge(28);
+    let userAgeInDays = userAge.age * 365;
     expect(userAgeInDays).toEqual(10220);
   });
+
+  it('should multiply user age by 365', function () {
+    let userAge = new UserAge(28);
+    let userAgeInDays = userAge.age * 365;
+    let mercuryAge = parseInt((userAgeInDays / 88).toFixed(0));
+    expect(mercuryAge).toEqual(117);
+  });
+
 });
