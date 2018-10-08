4aa867eab5e59b55869bb1d74979d53cf672f192
Brendan Shea <padma@email.com>
2018-10-07 18:33:15 -0700

    Create variables to calculate age on different planets.

--- src/main.js
+++ src/main.js
@@ -3,10 +3,25 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
+// Backend Logic:
+
+export function User(age, gender) {
+  this.age = age;
+  this.gender = gender;
+}
+
+// Frontend Logic:
+
 $(document).ready(function() {
   $("#form").submit(function(event) {
     event.preventDefault();
-    let userInput = parseInt($("#user-input").val());
-    let userAge = userInput * 365;
+    let newUser = new User();
+    newUser.age = parseInt($("#age-input").val());
+    newUser.gender = $("input:radio[name=gender]:checked").val();
+
+    let mercuryAge = parseInt(((newUser.age * 365) / 88).toFixed(0));
+    let venusAge = parseInt(((newUser.age * 365) / 225).toFixed(0));
+    let marsAge = parseInt(((newUser.age * 365) / 687).toFixed(0));
+    let jupiterAge = parseInt(((newUser.age * 365) / (11.8 * 365)).toFixed(0));
   });
 });
