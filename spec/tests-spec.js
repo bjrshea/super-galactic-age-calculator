9abb52f80b18a3f4b62c43012ac033b47d92cb35
Brendan Shea <padma@email.com>
2018-10-14 20:28:48 -0700

    Refactor to include planet age calculators in main constructor.

--- spec/tests-spec.js
+++ spec/tests-spec.js
@@ -1,43 +1,28 @@
 import UserInfo from './../src/UserInfo.js';
 
-let userAge = new User(28);
-let userAgeInDays = userAge.age * 365;
+const newUserInfo = new UserInfo(28, 'female');
 
-describe('User', function() {
+describe('UserInfo', function() {
 
-  it('should calculate user age', function() {
-    expect(userAge.age).toEqual(28);
-  });
-
-  it('should multiply user age by 365', function () {
-    expect(userAgeInDays).toEqual(10220);
+  it('should calculate user input', function() {
+    expect(newUserInfo.age).toEqual(28);
+    expect(newUserInfo.gender).toEqual('female');
+    expect(newUserInfo.gender).not.toEqual('male');
   });
 
   it('should calculate user age in Mercury years', function () {
-    let mercuryAge = parseInt((userAgeInDays / 88).toFixed(0));
-    expect(mercuryAge).toEqual(116);
+    expect(newUserInfo.mercury).toEqual(116);
   });
 
   it('should calculate user age in Venus years', function () {
-    let venusAge = parseInt((userAgeInDays / 225).toFixed(0));
-    expect(venusAge).toEqual(45);
+    expect(newUserInfo.venus).toEqual(45);
   });
 
   it('should calculate user age in Mars years', function () {
-    let marsAge = parseInt((userAgeInDays / 687).toFixed(0));
-    expect(marsAge).toEqual(15);
+    expect(newUserInfo.mars).toEqual(15);
   });
 
   it('should calculate user age in Jupiter years', function () {
-    let jupiterAge = parseInt((userAgeInDays / (11.8 * 365)).toFixed(0));
-    expect(jupiterAge).toEqual(2);
-  });
-
-  it('should calculate the amount of years left to live on a specific planet', function () {
-    let femaleLifeExpectancy = 81 * 365;
-    let maleLifeExpectancy = 76 * 365;
-    let testLifeExpectancy = maleLifeExpectancy - userAgeInDays;
-    let mercuryLifeExpectancy = parseInt((testLifeExpectancy / 88).toFixed(0));
-    expect(mercuryLifeExpectancy).toEqual(199);
+    expect(newUserInfo.jupiter).toEqual(2);
   });
 });
--- src/UserInfo.js
+++ src/UserInfo.js
@@ -1,28 +1,32 @@
 export default class UserInfo {
-
-  function User(age, gender) {
+  
+  constructor(age, gender) {
     this.age = age;
     this.gender = gender;
+    this.mercury = parseInt(((this.age * 365) / 88).toFixed(0));
+    this.venus = parseInt(((this.age * 365) / 225).toFixed(0));
+    this.mars = parseInt(((this.age * 365) / 687).toFixed(0));
+    this.jupiter = parseInt(((this.age * 365) / (11.8 * 365)).toFixed(0));
   }
 
-  function PlanetAge (mercury, venus, mars, jupiter) {
-    this.mercury = mercury;
-    this.venus = venus;
-    this.mars = mars;
-    this.jupiter = jupiter;
-  }
-
-  function MaleLifeExpectancy (mercury, venus, mars, jupiter) {
-    this.mercury = mercury;
-    this.venus = venus;
-    this.mars = mars;
-    this.jupiter = jupiter;
-  }
-
-  function FemaleLifeExpectancy (mercury, venus, mars, jupiter) {
-    this.mercury = mercury;
-    this.venus = venus;
-    this.mars = mars;
-    this.jupiter = jupiter;
-  }
+  // function PlanetAge (mercury, venus, mars, jupiter) {
+  //   this.mercury = mercury;
+  //   this.venus = venus;
+  //   this.mars = mars;
+  //   this.jupiter = jupiter;
+  // }
+  //
+  // function MaleLifeExpectancy (mercury, venus, mars, jupiter) {
+  //   this.mercury = mercury;
+  //   this.venus = venus;
+  //   this.mars = mars;
+  //   this.jupiter = jupiter;
+  // }
+  //
+  // function FemaleLifeExpectancy (mercury, venus, mars, jupiter) {
+  //   this.mercury = mercury;
+  //   this.venus = venus;
+  //   this.mars = mars;
+  //   this.jupiter = jupiter;
+  // }
 }
